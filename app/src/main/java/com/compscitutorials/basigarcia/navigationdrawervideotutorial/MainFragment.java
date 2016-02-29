85a830c5c22fd83b2fbec3a9107a52c7491af6cc
yurareutskiy <yurareutskiy@gmail.com>
2016-02-29 16:18:51 +0300

    init

---
+++ app/src/main/java/com/compscitutorials/basigarcia/navigationdrawervideotutorial/MainFragment.java
@@ -0,0 +1,37 @@
+package com.compscitutorials.basigarcia.navigationdrawervideotutorial;
+
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+
+}
